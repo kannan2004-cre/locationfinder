d81177b145bb77fc867231a88fce2a7ab60cbb46
kannan2004-cre <83084342+kannan2004-cre@users.noreply.github.com>
2024-02-11 12:13:47 +0530

    phonelocatio.py

--- phonelocatio.py
+++ phonelocatio.py
@@ -1,5 +1,5 @@
 import phonenumbers
 from phonenumbers import geocoder
-phone_number = phonenumbers.parse("+9188483 74167")
+phone_number = phonenumbers.parse(//enter phone number with country code in double codes)
 print("\nphone number location is\n")
 print(geocoder.description_for_number(phone_number, "en"))
